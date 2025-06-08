71df65a4a05c2396bb9a3627895df9300579fa3f
Ahmedsabbah121 <ahmed_Sabbah121@outlook.com>
2025-06-08 18:36:45 +0300

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Run migrations automatically (ONLY FOR TESTING)
+        if (app()->environment('production')) {
+            try {
+                Artisan::call('migrate', ["--force" => true]);
+            } catch (\Exception $e) {
+                \Log::error("Migration failed: " . $e->getMessage());
+            }
+        }
     }
 }
